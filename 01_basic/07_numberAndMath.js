99470223e44c24335ed9744094b7ff0d1b8ac3d0
Vikram Kumar Modi <123832849+Vikrammodi18@users.noreply.github.com>
2025-01-31 22:53:44 +0000

    number and math

---
+++ 01_basic/07_numberAndMath.js
@@ -0,0 +1,25 @@
+const score = 50
+/*
+console.log(typeof score)
+
+const age = new Number(18.234352)
+console.log(age)
+const amount = 1000000
+console.log(typeof age)
+console.log(age.toFixed(2)) //toFixed() are used to diplay precigion number in specific length
+console.log(amount.toLocaleString('en-IN'))  //toLocaleString are used to separate number by comma generally used in to display amount
+*/
+
+//+++++++++++ Maths +++++++++++++++
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.4))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.9))
+// console.log(Math.sqrt(26).toFixed(2))
+// console.log(Math.max(23,4,5,63,2))
+// console.log(Math.min(23,4,5,63,2))
+
+const max = 30
+const min = 15
+
+console.log(  Math.floor(Math.random() * (max - min +1)) + min )
