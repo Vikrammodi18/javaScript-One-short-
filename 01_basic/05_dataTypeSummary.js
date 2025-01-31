9d58ae720dc4a5e0098122122919309d7b458b54
Vikram Kumar Modi <123832849+Vikrammodi18@users.noreply.github.com>
2025-01-31 21:22:49 +0000

    datatypeSummary

---
+++ 01_basic/05_dataTypeSummary.js
@@ -0,0 +1,18 @@
+// data type are divided into how they are stored and access them
+// 1.primitive //call by value
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt
+
+
+
+// 2. non-premitive //reference
+// array, Objects, functions 
+
+let hero = ['shaktiman','spiderman']
+let myobj = {
+    name:"vikram",
+    age:19,
+}
+let myfunc = ()=>{
+    console.log("hello world")
+}
+console.log(typeof myfunc) //functions
