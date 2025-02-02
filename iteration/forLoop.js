3e0e4794ccbe0aedae502531db4a6a6c03f34de8
Vikram Kumar Modi <123832849+Vikrammodi18@users.noreply.github.com>
2025-02-02 23:17:33 +0000

    implementation of for loop

---
+++ iteration/forLoop.js
@@ -0,0 +1,27 @@
+// let arr = [1,2,3,4]
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+
+//continue and break
+
+//continue are use to escape from a particular condition or implementation of continue did not allow to excute the rest of the block of code
+//break terminate the iteration 
+// for(let i =1;i<=10;i++){
+
+//     if(i === 5){
+//         break
+
+//     }
+//        console.log(i)
+  
+// }
+// for(let i =1;i<=10;i++){
+
+//     if (i === 5){
+//         continue
+//         console.log(i)
+//     }
+  
+// }
