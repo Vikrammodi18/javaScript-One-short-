261dd1a52261b675c3ddb0aa8370198b5b30bea4
Vikram Kumar Modi <123832849+Vikrammodi18@users.noreply.github.com>
2025-02-05 13:05:33 +0000

    for in loo[ and for of loop

--- iteration/highOrderLoop.js
+++ iteration/highOrderLoop.js
@@ -5,28 +5,38 @@ let arr = [1,2,3,4,4,5,6]
 // }
 
 // map only store unique key, we can itirate over map :- for(let val of mymap){}
-// let myMap = new Map()
-// myMap.set('IN',"India")
-// myMap.set('US',"USA")
-// myMap.set('CA',"Canada")
-// myMap.set('Ban',"Bangladesh")
+let myMap = new Map()
+myMap.set('IN',"India")
+myMap.set('US',"USA")
+myMap.set('CA',"Canada")
+myMap.set('Ban',"Bangladesh")
 
-// console.log(myMap)
-// myMap.set('IN',"Indo")
-// console.log(myMap)
+console.log(myMap)
+myMap.set('IN',"Indo")
+console.log(myMap)
+//more methods like map.keys(), .values()
+for(let key of myMap.keys()){
+    console.log(key)
+}
 
-// for(let [key,val] of myMap){
-//     console.log(key,val)
+
+// let myObj = {
+//     "js":'javaScript',
+//     c:"c",
+//     py:"python",
+//     rb:"ruby"
 // }
 
+// for(let key in myObj){
+//     console.log(myObj[key])
+// }
 
-let myObj = {
-    "js":'javaScript',
-    c:"c",
-    py:"python",
-    rb:"ruby"
-}
+const programming = ["js","rb","py","java","cpp"]
+// for(let key in programming){
+//     console.log(key)
+//     // console.log(programming[key])
+// }
 
-for(let key in myObj){
-    console.log(myObj[key])
-}
+// for(let key of programming){
+//     console.log(key)
+// }
